4270adbaf25a444400be618147baaeeaa6052acc
samoncikja23 <samoncikja23@amityschools.org>
2022-09-29 09:06:32 -0400

    first commit

---
+++ face/src/Face.java
@@ -0,0 +1,10 @@
+public class Face {
+    public static void main(String[] args) throws Exception {
+        System.out.println("  >>>>>");
+        System.out.println(" +'''''+");
+        System.out.println("(| 0  0|)");
+        System.out.println(" |   ^ |");
+        System.out.println(" |  U  |");
+        System.out.println(" +-----+");
+    }
+}
